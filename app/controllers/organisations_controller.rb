d9b81fc621b24ff5b83a2f8b090a441097973463
Devansh <devansh.guddeti1998@gmail.com>
2022-07-09 17:42:04 -0400

    Initial commit. Create Organisations and Create Users. Implement Devise Add and Join Organisation

---
+++ app/controllers/organisations_controller.rb
@@ -0,0 +1,59 @@
+class OrganisationsController < ApplicationController
+  before_action :set_organization, only: [:show, :edit, :update, :destroy, :join, :leave]
+  before_action :init_organization, only: [:index, :new]
+
+  def index
+    @organisations = Organisation.all
+  end
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+
+    if @organisation.save
+      redirect_to @organisation, notice: "Successfully created organisation"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @organisation.update(organisation_params)
+      redirect_to @organisation, notice: "Successfully updated organisation"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @organisation.destroy
+    redirect_to root_path, status: :see_other, notice: "Successfully destroyed organisation"
+  end
+
+  def join
+    current_user.update(organisation_id: params[:id])
+
+    redirect_to root_path, status: :see_other, notice: "Successfully Joined organisation"
+  end
+
+  def leave
+    current_user.update(organisation_id: nil)
+
+    redirect_to root_path, status: :see_other, notice: "Successfully Left organisation"
+  end
+
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name, :hourly_rate)
+  end
+
+  def set_organization
+    # binding.pry
+    @organisation = Organisation.find(params[:id])
+  end
+
+  def init_organization
+    @organisation = Organisation.new
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root to: "organisations#index"
+
+  devise_for :users do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  resources :organisations do
+    member do
+      put :join
+      put :leave
+    end
+  end
+end
