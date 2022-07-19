8f215ac310ed1f901cc4c4af0d54456a4680c9b0
Devansh <devansh.guddeti1998@gmail.com>
2022-07-19 09:50:35 -0400

    Rspec test for Login

---
+++ spec/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe User, type: :model do
+  subject do
+    described_class.new(
+      password: "password",
+      email: "john@doe.com"
+    )
+  end
+
+  describe "Validations" do
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a password" do
+      subject.password = "asdfg"
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      subject.email = "johndoe"
+      expect(subject).to_not be_valid
+    end
+  end
+end
